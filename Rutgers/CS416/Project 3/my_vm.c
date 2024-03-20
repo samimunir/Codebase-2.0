98204ada232dc5ec84d722e4778468c56e25000d
Sami Munir <smunir01@outlook.com>
2024-03-20 08:02:59 -0400

    updating data structure comments & adding physical & virtual memory bitmaps

--- Rutgers/CS416/Project 3/my_vm.c
+++ Rutgers/CS416/Project 3/my_vm.c
@@ -3,6 +3,9 @@
 //TODO: Define static variables and structs, include headers, etc.
 /*
     Page frame structure
+    - this structure effectively tracks the allocated state and
+        optionally the reference count for shared pages, along
+        with a pointer to the data itself.
 */
 typedef struct page_frame {
     /*
@@ -21,6 +24,8 @@ typedef struct page_frame {
 
 /*
     Page table entry (PTE)
+    - this structure includes the valid bit and physical page number
+        for efficient virtual-to-physical address translation.
 */
 typedef struct pte {
     /*
@@ -40,6 +45,10 @@ typedef struct pte {
 /*
     Page table (one-level)
     - assuming a page size of 4KB
+    - this structure defines a one-level page table using an array
+        of PTEs.
+        > can be modified to a multi-level structure if needed based
+            on address space requirements.
 */
 # define PAGE_SIZE 4096
 typedef struct page_table {
@@ -51,6 +60,8 @@ typedef struct page_table {
 
 /*
     Page directory entry (PDE)
+    - this structure defines a page directory entry with a pointer
+        to a child page table and a valid bit.
 */
 typedef struct pde {
     /*
@@ -69,6 +80,8 @@ typedef struct pde {
 
 /*
     Top-level page directory.
+    - this structure defines the top-level page directory using an
+        array of PDEs.
 */
 #define NUM_PT_ENTRIES (MAX_MEMSIZE / (PAGE_SIZE * sizeof(PageTable)))
 typedef struct page_directory {
@@ -77,6 +90,62 @@ typedef struct page_directory {
     */
     PDE entries[NUM_PT_ENTRIES];
 } PageDirectory;
+
+/*
+    Translation lookaside buffer (TLB) entry.
+    - this structure defines an entry of the TLB, storing the virtual
+        and physical page numbers for faster translation.
+*/
+typedef struct tlb_entry {
+    /*
+        Virtual page number.
+    */
+    unsigned int vpn;
+    /*
+        Physical page number.
+    */
+    unsigned int pfn;
+} TLBEntry;
+
+/*
+    Two separate bitmaps to track free and allocated pages
+        efficiently.
+    - physical_mem_bitmap
+        > tracks free and allocated physical memory pages.
+    - virtual_mem_bitmap
+        > tracks free and allocated virtual memory pages
+            (page table entries).
+*/
+/*
+    Size of bitmap in bytes.
+*/
+#define BITMAP_SIZE(memsize) ((memsize) / 8)
+/*
+    Physical memory bitmap.
+*/
+char *physical_mem_bitmap;
+/*
+    Virtual memory bitmap.
+*/
+char *virtual_mem_bitmap;
+/*
+    Function to set a bit in the bitmap (1 indicates allocated).
+*/
+void set_bit(char *bitmap, unsigned int bit_num) {
+    bitmap[bit_num / 8] |= (1 << (bit_num % 8));
+}
+/*
+    Function to clear a bit in the bitmap (0 indicates free).
+*/
+void clear_bit(char *bitmap, unsigned int bit_num) {
+    bitmap[bit_num / 8] &= ~(1 << (bit_num % 8));
+}
+/*
+    Function to check if a bit is set (1) in the bitmap.
+*/
+int test_bit(char *bitmap, unsigned int bit_num) {
+    return (bitmap[bit_num / 8] >> (bit_num % 8)) & 1;
+}
 
 void set_physical_mem(){
     //TODO: Finish
