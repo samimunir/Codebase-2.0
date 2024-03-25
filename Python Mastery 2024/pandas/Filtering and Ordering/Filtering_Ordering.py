949d11b8e349353e13efc198574d0f03af8e58ca
Sami Munir <smunir01@outlook.com>
2024-03-24 23:08:30 -0400

    loading in pandas dataframes

--- Python Mastery 2024/pandas/Filtering and Ordering/Filtering_Ordering.py
+++ Python Mastery 2024/pandas/Filtering and Ordering/Filtering_Ordering.py
@@ -6,3 +6,10 @@
 import pandas as pd
 
 print('Filtering & Ordering Data.\n')
+
+csv_data_frame = pd.read_csv(r'data\ratingsSample.csv', header=None, names=['USER', 'MOVIE', 'RATING'])
+print('csv_data_frame ->\n----------------------\n', csv_data_frame)
+text_data_frame = pd.read_table(r'data\moviesSample.txt', sep='|')
+print('\ntext_data_frame ->\n----------------------\n', text_data_frame)
+text_2_date_frame = pd.read_table(r'data\moviesSample_2.txt', sep='|')
+print('\ntext_2_data_frame ->\n----------------------\n', text_2_date_frame)
