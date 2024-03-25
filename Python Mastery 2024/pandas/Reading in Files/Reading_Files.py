502a6948530c704be4ec72c012a3ca3c59285a42
Sami Munir <smunir01@outlook.com>
2024-03-24 22:52:11 -0400

    reading in files with pandas complete

--- Python Mastery 2024/pandas/Reading in Files/Reading_Files.py
+++ Python Mastery 2024/pandas/Reading in Files/Reading_Files.py
@@ -11,13 +11,18 @@ print('Reading in Files with Pandas.\n')
 csv_data_frame = pd.read_csv(r'data\ratingsSample.csv', header=None, names=['USER', 'MOVIE', 'RATING'])
 # ratingSample.csv -> [user_ID, movie_ID, movie_rating (0.0-5.0)]
 print('csv_data_frame ->\n----------------------\n', csv_data_frame)
-print(csv_data_frame.info())
+print(csv_data_frame.info(), '\n')
+# print(csv_data_frame['USER'], '\n')
+print(csv_data_frame['MOVIE'], '\n')
+# print(csv_data_frame['RATING'], '\n')
+print(csv_data_frame.loc[29], '\n')
+print(csv_data_frame.iloc[29], '\n')
 
 # use '|' as a delimiter/separator to parse data into rows/columns.
 text_data_frame = pd.read_table(r'data\moviesSample.txt', sep='|')
-print('\ntext_data_frame ->\n----------------------\n', text_data_frame)
-print(text_data_frame.info())
+# print('\ntext_data_frame ->\n----------------------\n', text_data_frame)
+# print(text_data_frame.info())
 
 text_2_date_frame = pd.read_table(r'data\moviesSample_2.txt', sep='|')
-print('\ntext_2_data_frame ->\n----------------------\n', text_2_date_frame)
-print(text_2_date_frame.info())
+# print('\ntext_2_data_frame ->\n----------------------\n', text_2_date_frame)
+# print(text_2_date_frame.info())
