f6b56711d95505bd828a0788d085974cd2de9805
Sami Munir <smunir01@outlook.com>
2024-03-24 22:26:52 -0400

    adding data directory & reading in .csv file

--- Python Mastery 2024/pandas/Reading in Files/Reading_Files.py
+++ Python Mastery 2024/pandas/Reading in Files/Reading_Files.py
@@ -4,3 +4,10 @@
 # pandas
 # Reading_Files.py
 import pandas as pd
+
+print('Reading in Files with Pandas.\n')
+
+# use 'r' to read in file path as raw text.
+data_frame = pd.read_csv(r'data\ratingsSample.csv', header=None, names=['USER', 'MOVIE', 'RATING'])
+# ratingSample.csv -> [user_ID, movie_ID, movie_rating (0.0-5.0)]
+print('data_frame ->\n', data_frame)
