922a0fffb74aa9d6e9702a9b34b557a18eebc8fe
Sami Munir <smunir01@outlook.com>
2024-03-25 23:54:39 -0400

    implementing pandas helper functions

--- Python Mastery 2024/Aircraft Data Analytics Project/Aircrafts_Analysis.py
+++ Python Mastery 2024/Aircraft Data Analytics Project/Aircrafts_Analysis.py
@@ -3,8 +3,34 @@
 # Aircraft Data Analytics Project
 # - Aircrafts_Analysis.py
 # Data: Aircrafts_Data.csv
-# - AC_ID, AC_NAME, AC_TYPE_CODE, AC_REG,
+# - AC_ID, AC_NAME, AC_CODE, AC_REG,
 # - AC_AIRLINE, ENG_TYPE
 # (in that order left -> right)
 
+import pandas as pd
+
 print('\nAircrafts_Analysis.py\n')
+
+FILE_PATH = r'data\Aircrafts_Data.csv'
+
+def load_and_print_dataframe():
+    aircrafts_dataframe = pd.read_csv(FILE_PATH)
+    print('aircrafts_dataframe ->\n----------------------\n', aircrafts_dataframe)
+    return aircrafts_dataframe
+
+def print_dataframe_info(aircrafts_dataframe):
+    print('\naircrafts_dataframe.info() ->\n-----------------------------')
+    print(aircrafts_dataframe.info())
+
+def get_aircrafts_from_airline(aircrafts_dataframe, airline):
+    filtered_aircrafts_dataframe = aircrafts_dataframe[(aircrafts_dataframe['AC_AIRLINE'] == airline)]
+    print(f'\naircrafts from airline: {airline} ->\n------------------------------------------')
+    print(filtered_aircrafts_dataframe)
+    return filtered_aircrafts_dataframe
+
+def exec_pandas():
+    aircrafts_dataframe = load_and_print_dataframe()
+    # print_dataframe_info(aircrafts_dataframe=aircrafts_dataframe)
+    get_aircrafts_from_airline(aircrafts_dataframe=aircrafts_dataframe, airline='United Airlines')
+
+exec_pandas()
