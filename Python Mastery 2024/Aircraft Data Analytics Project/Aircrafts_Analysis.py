5ff57cc790e322fb552affa15da61e3fbc9b50cf
Sami Munir <smunir01@outlook.com>
2024-03-25 22:55:42 -0400

    adding data directory (Aircrafts_Data.csv)

---
+++ Python Mastery 2024/Aircraft Data Analytics Project/Aircrafts_Analysis.py
@@ -0,0 +1,10 @@
+# Sami Munir
+# March 25th, 2024
+# Aircraft Data Analytics Project
+# - Aircrafts_Analysis.py
+# Data: Aircrafts_Data.csv
+# - AC_ID, AC_NAME, AC_TYPE_CODE, AC_REG,
+# - AC_AIRLINE, ENG_TYPE
+# (in that order left -> right)
+
+print('\nAircrafts_Analysis.py\n')
