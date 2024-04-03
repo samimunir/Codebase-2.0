8b4c65b1c0f2c684933be71990c7a8e50c5e4a50
Sami Munir <smunir01@outlook.com>
2024-04-03 02:11:16 -0400

    starting data structure & algoriths in C#

---
+++ C# Data Structures and Algorithms/Hello World/Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+    Sami Munir
+    April 2nd, 2024
+    C# Data Structures & Algorithms
+    Hello World program
+    - Hello World
+        > Program.cs
+*/
+
+Console.WriteLine("Hello world!");
+int x = 2;
+int y = 5;
+int z = x + y;
+Console.WriteLine("x = " + x + ", y = " + y);
+Console.WriteLine("z = x + y = " + z);
