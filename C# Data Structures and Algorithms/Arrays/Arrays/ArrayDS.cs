5cbb9dd1acbdeca24295c07ea82f1787346f7c50
Sami Munir <smunir01@outlook.com>
2024-04-04 04:44:38 -0400

    adding function PrintDS()

--- C# Data Structures and Algorithms/Arrays/Arrays/ArrayDS.cs
+++ C# Data Structures and Algorithms/Arrays/Arrays/ArrayDS.cs
@@ -6,12 +6,26 @@ public class ArrayDS {
     public ArrayDS() {
         capacity = 5;
         main_array = new int[capacity];
-        Console.WriteLine("\nSuccessfully initialized main_array[] to capacity [" + capacity + "].\n\tpointer: " + pointer);
+        Console.WriteLine("\nSuccessfully initialized main_array[] to capacity [" + capacity + "].");
+        PrintDS();
     }
 
     public ArrayDS(int capacity) {
         this.capacity = capacity;
         main_array = new int[capacity];
-        Console.WriteLine("\nSuccessfully initialized main_array[] to capacity [" + capacity + "].\n\tpointer: " + pointer);
+        Console.WriteLine("\nSuccessfully initialized main_array[] to capacity [" + capacity + "].");
+        PrintDS();
+    }
+
+    public void PrintDS() {
+        if (main_array == null) {
+            Console.WriteLine("main_array[]: []");
+            Console.WriteLine("\tpointer: " + pointer);
+        } else {
+            Console.Write("main_array[]: [");
+            main_array.ToList().ForEach(i => Console.Write(" " + i.ToString()));
+            Console.Write(" ]\n");
+            Console.WriteLine("\tpointer: " + pointer);
+        }
     }
 }
--- C# Data Structures and Algorithms/Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ C# Data Structures and Algorithms/Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e0319c62f70def5a1a1aa5f74543f86161f1910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6464eff88b58d9f8d9cf994eea2f7b0015a6be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
