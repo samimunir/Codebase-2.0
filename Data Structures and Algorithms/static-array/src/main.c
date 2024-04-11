7e51bdc2c8bef79ecd69ac3055a2a76dd96724bd
Sami Munir <smunir01@outlook.com>
2024-04-11 03:33:47 -0400

    restarting dsalgo directory

---
+++ Data Structures and Algorithms/static-array/src/main.c
@@ -0,0 +1,16 @@
+/*
+    Sami Munir
+    April 11th, 2024
+    Codebase 2.0
+    Data Structures & Algorithms
+    > static-array
+    > C implementation
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("\nstatic-array implementation in C\n");
+    return EXIT_SUCCESS;
+}
